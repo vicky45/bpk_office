8b87b4898a9f19bcc6bc52754d65d1ff021037bc
nanda_unbraw <ananda_alfa@student.ub.ac.id>
2019-07-23 15:09:15 +0700

    change controller

--- app/Http/Controllers/C_Event.php
+++ app/Http/Controllers/C_Event.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Auth;
 use App\EventModel;
 use App\Join_EventModel;
 use App\QuestionModel;
-use auth;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 
 class C_Event extends Controller {
 
@@ -15,86 +15,72 @@ class C_Event extends Controller {
         $this->middleware('auth');
     }
 
-    public function SitanyaPage() {
-        return view('sitanyahome');
+    public function TanjaHome() {
+        return view('tanjahome');
     }
 
-    public function switch_event(Request $request) {
-        $request->session()->forget('event');
-        return redirect('/sitanyahome');
+    public function create() {//create some page create
+        // Not Use
     }
 
-    public function join_event(Request $request) {
-        $code = $request->input('join');
+    public function store(Request $request) {//insert data
+        $user = Auth::user()->name;
+        $code = $request->join;
         $status = 1;
-        $event = EventModel :: where('code_event', '=', $code)->get();
-        $domain = Auth::user()->name;
-        if ($event != null) :
-            foreach ($event as $tmp) {//get data
-                //inisialisasi data
-                $auth = $tmp->user_created;
+        if ($code === null):
+            //Button Create Event
+            $codeevent = Str::random(6);
+            $this->validate($request, [
+                'event' => 'required',
+                'location' => 'required',
+                'date' => 'required',
+                'time' => 'required',
+            ]);
+
+            $EventModel = EventModel::create([
+                        'code_event' => $codeevent,
+                        'name_event' => $request->event,
+                        'date_event' => $request->date,
+                        'time_event' => $request->time,
+                        'status_event' => $status,
+                        'location' => $request->location,
+                        'user_created' => $user
+            ]);
+            if ($EventModel->exists):
+                $request->session()->forget('event');
+                $request->session()->put('event', $codeevent);
+                return redirect()->route('tanja.show', ['codeevent' => $codeevent]);
+            else:
+                return redirect()->back()->with(['warning' => 'Create Event Failed!']);
+            endif;
+        else:
+            //button join event
+            $event = EventModel :: where('code_event', '=', $code)->get();
+            if ($event != null) :
+                foreach ($event as $tmp) {//get data
+                    //inisialisasi data
+                    $auth = $tmp->user_created;
+                    $idEvent = $tmp->idEvent;
 //                seleksi admin
-                if ($domain == $auth):
-                    $EventModel = EventModel::find($tmp->idEvent);
-                    $EventModel->status_event = $status;
-                    $EventModel->save();
-                    $request->session()->put('event', $code);
-                    return redirect('/homeadmin');
-                else:
-                    $request->session()->put('event', $code);
-                    return redirect('/homeuser');
-                endif;
-            }
+                    $request->session()->forget('event');
+                    if ($user === $auth):
+                        $EventModel = EventModel::find($idEvent);
+                        $EventModel->status_event = $status;
+                        $EventModel->save();
+                        $request->session()->put('event', $code);
+                        return redirect('/homeadmin');
+                    else:
+                        $request->session()->put('event', $code);
+                        return redirect('/homeuser');
+                    endif;
+                }
+            endif;
+            return redirect()->back()->with(['warning' => 'Code Tidak Terdaftar!']);
         endif;
-        return redirect()->back()->with(['warning' => 'Code Tidak Terdaftar!']);
     }
-
-    public function create_event(Request $request) {
-        $codeevent = Str::random(6);
-        $status = 1;
-        $usercreated = Auth::user()->name;
-        $this->validate($request, [
-            'event' => 'required',
-            'location' => 'required',
-            'date' => 'required',
-            'time' => 'required',
-        ]);
-
-        $EventModel = EventModel::create([
-                    'code_event' => $codeevent,
-                    'user_created' => $usercreated,
-                    'name_event' => $request->event,
-                    'location' => $request->location,
-                    'date_event' => $request->date,
-                    'time_event' => $request->time,
-                    'status_event' => $status
-        ]);
-        if ($EventModel->exists) {
-            $request->session()->forget('event');
-            $request->session()->put('event', $codeevent);
-            return redirect('/homeadmin');
-        }
-        return redirect('/sitanyahome');
-    }
-
-    public function update_event(Request $request) {
-        $usercreated = Auth::user()->name;
-        $idEvent = $request->id;
-        // update data pegawai
-        $EventModel = EventModel::find($idEvent);
-        $EventModel->code_event = $request->code;
-        $EventModel->user_created = $usercreated;
-        $EventModel->name_event = $request->event;
-        $EventModel->location = $request->location;
-        $EventModel->date_event = $request->date;
-        $EventModel->time_event = $request->time;
-        $EventModel->save();
-        if ($EventModel->exists) {
-            $request->session()->forget('event');
-            $request->session()->put('event', $request->code);
-            return redirect('/homeadmin');
-        }
-        return redirect('/homeadmin');
+    
+    public function destroy($id) {//hapus
+        //`
     }
 
     public function user_event(request $request) {// /homeuser
@@ -115,19 +101,19 @@ class C_Event extends Controller {
                     $idEv = $ev->idEvent;
                     $nip = $ev->NIP;
                 }
-                $question = QuestionModel::where('NIP','=',$nip)->get();
+                $question = QuestionModel::where('NIP', '=', $nip)->get();
                 if ($nip === Auth::user()->NIP && $idEvent === $idEv) {
-                    return view('homeuser', ['event' => $event],['question' => $question]);
+                    return view('homeuser', ['event' => $event], ['question' => $question]);
                 } else {
                     Join_EventModel::create([
                         'idEvent' => $idEvent,
                         'NIP' => Auth::user()->NIP
                     ]);
-                    return view('homeuser', ['event' => $event],['question' => $question]);
+                    return view('homeuser', ['event' => $event], ['question' => $question]);
                 }
             }
         }
-        return redirect('/sitanyahome')->with(['warning' => 'Input code and Join Required!']);
+        return redirect('/home')->with(['warning' => 'Input code and Join Required!']);
     }
 
     public function admin_event(request $request) {// /homeadmin
@@ -143,51 +129,75 @@ class C_Event extends Controller {
                 $user = $ev->user_created;
             }
             if ($domainB === $user) {
-                $cek = Join_EventModel :: where('NIP', '=', $domainB)->get();
+                $cek = Join_EventModel :: where('NIP', '=', $domainA)->get();
                 if ($cek != null) {
                     //cek nip
                     foreach ($cek as $ev) {
                         $idEv = $ev->idEvent;
                         $nip = $ev->NIP;
                     }
-                    if ($nip === $domainB && $idEvent === $idEv) {
-                        return view ('homeadmin',['event' => $event]);
+                    if ($nip === $domainA && $idEvent === $idEv) {
+                        return view('homeadmin', ['event' => $event]);
                     } else {
                         Join_EventModel::create([
                             'idEvent' => $idEvent,
                             'NIP' => Auth::user()->NIP
                         ]);
-                        return view ('homeadmin',['event' => $event]);
+                        return view('homeadmin', ['event' => $event]);
                     }
                 }
             }
         }
-        return redirect('/sitanyahome')->with(['warning' => 'Join from Code admin Required!']);
+        return redirect('/home')->with(['warning' => 'Join from Code admin Required!']);
     }
-    //=================================================================//
-//                                End @Ananda
-    public function tampilkanSession(Request $request) {
-		if($request->session()->has('event')){
-			echo $request->session()->get('event');
-		}else{
-			echo 'Tidak ada data dalam session.';
-		}
-	}
- 
-	// membuat session
-	public function buatSession(Request $request) {
-		$request->session()->put('event','BPKRI');
-		echo "Data telah ditambahkan ke session.";
-	}
- 
-	// menghapus session
-	public function hapusSession(Request $request) {
-		$request->session()->forget('event');
-		echo "Data telah dihapus dari session.";
-	}
+
+    /*     * ===============================================================================================**\
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(Request $request, $codeevent) {//tampil admin atau user berdasarkan code (Joint_E
         
-        public function cekapi(){
-            $cekapi = EventModel::all();
-            return cekapi;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {//update data berdasar
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {//update berdasar
+        //
+    }
+
+    
+
+//    ===test case===  \\
+    public function tampilkanSession(Request $request) {
+        if ($request->session()->has('event')) {
+            echo $request->session()->get('event');
+        } else {
+            echo 'Tidak ada data dalam session.';
         }
+    }
+
+    // menghapus session
+    public function hapusSession(Request $request) {
+        $request->session()->forget('event');
+        echo "Data telah dihapus dari session.";
+    }
+
 }
