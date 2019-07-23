5bbcdc4698dc90812ec1cf56ed9e4922ab310465
nanda_unbraw <ananda_alfa@student.ub.ac.id>
2019-07-23 14:57:00 +0700

    change controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,21 @@
 <?php
-use Illuminate\Support\Str;
-
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes TANJA | Badan Pemeriksa Keuangan
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
 Route::get('/', function () { //Localhost master default link
-    return view('HomeGuest');
+    return view('satubpkhome');
 });
-
-
-Auth ::routes();
-Route::get('/sitanyahome','C_Event@SitanyaPage')->name('sitanyahome');
-
+Auth::routes();
+Route::get('/home', 'C_Event@TanjaHome')->name('tanjahome');
 //route admin
 Route::get('/homeadmin', 'C_Event@admin_event');
 //route user
 Route::get('/homeuser', 'C_Event@user_event');
-
-
-//route create
-Route::post('/create','C_Event@create_event');//created_user
-//route join
-Route::post('/join','C_Event@join_event');//user used or created_user
-//route switch event
-Route::get('/switch_event','C_Event@switch_event');
-
-//update event
-Route::post('/update','C_Event@update_event');
-
+Route::resource('tanja','C_Event',[
+    'only' => ['store','show']
+    ]);
+//==test case==//
 Route::get('/session/tampil','C_Event@tampilkanSession');
-Route::get('/session/buat','C_Event@buatSession');
 Route::get('/session/hapus','C_Event@hapusSession');
-
-//add speaker
-Route::post('/add_speaker','C_Question@speaker_add');
-//delete speaker
-Route::get('/delete_speaker/{id}','C_Question@speaker_delete');
-//ask question
-Route::post('/ask','C_Question@ask');
-
-Route::get('/cekapi','C_Event@cekapi');
